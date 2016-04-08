f88bb0aec51c789677dac85f4dd8ab3bd984fcf2
Qu Dong <qudong452@gmail.com>
2016-04-08 20:14:43 +0800

    sigmoid.m function fix for MATLAB
    Signed-off-by: Qu Dong <qudong452@gmail.com>

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -8,11 +8,7 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-for i=1:size(z)(1)
-    for j=1:size(z)(2)
-        g(i,j) = 1 / (1 + exp(-z(i,j))); % negative before z!!!!!
-    end
-end
+g = 1.0 ./ (1.0 + exp(-z));
 % =============================================================
 
 end
